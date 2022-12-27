738cfc30604a0147f200d89bd65f8bfcec41a36f
lemur <git-private.7axsu@simplelogin.com>
2022-12-27 19:13:30 -0300

    feat: add coin distribution

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Use at your own risk.
 ## Usage
 
 Duplicate `.env.example`, rename to `.env`, fill the variables and `yarn start`. Set the provider to `http://127.0.0.1:8545` and run `yarn anvil` to start a local blockchain for testing.
+
+Example .env
+
+```
+PROVIDER=http://127.0.0.1:8545
+PRIVATE_KEY=0xyourkey
+TARGET_NONCE=200
+BENEFICIARIES=0xdea...dbe,0xc0f...fee
+AMOUNT_PER_BENEFICIARY=0.1,0.2
+```
+
+`AMOUNT_PER_BENEFICIARY` is in ether.
--- index.ts
+++ index.ts
@@ -6,9 +6,26 @@ dotenv.config();
 
 const PROVIDER_URL = process.env.PROVIDER;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const BENEFICIARIES = process.env.BENEFICIARIES?.split(`,`);
+const AMOUNT_PER_BENEFICIARY = process.env.AMOUNT_PER_BENEFICIARY?.split(`,`);
+const CONFIRMATIONS = 5;
 
 async function main() {
-  if (!PROVIDER_URL || !PRIVATE_KEY) {
+  if (
+    !PROVIDER_URL ||
+    !PRIVATE_KEY ||
+    !AMOUNT_PER_BENEFICIARY ||
+    !BENEFICIARIES
+  ) {
+    console.error('Missing variables.');
+    process.exit(1);
+  }
+
+  if (
+    AMOUNT_PER_BENEFICIARY.length != BENEFICIARIES.length ||
+    AMOUNT_PER_BENEFICIARY.length === 0
+  ) {
+    console.error('Mismatch in beneficiaries and amounts');
     process.exit(1);
   }
 
@@ -53,7 +70,7 @@ async function main() {
     console.info(
       `Increasing next nonce to ${nextTxNonce + 1} sent. Waiting for confs...`
     );
-    await tx.wait(5);
+    await tx.wait(CONFIRMATIONS);
     console.info('Nonce increased');
     console.info();
 
@@ -65,7 +82,7 @@ async function main() {
   );
   const factory = new ethers.ContractFactory(abi, bytecode, wallet);
   const contract = await factory.deploy({ nonce: targetNonce });
-  await contract.deployTransaction.wait();
+  await contract.deployTransaction.wait(CONFIRMATIONS);
 
   const walletBalanceAfter = await wallet.getBalance();
   console.info(`Done. `);
@@ -73,6 +90,31 @@ async function main() {
     `Recovered approximately ${ethers.utils.formatUnits(
       walletBalanceAfter.sub(walletBalanceBefore)
     )} coins`
+  );
+
+  // Distribute recovered funds.
+  console.info('Distributing funds.');
+  console.info(
+    "Don't stop the proram or be carefull with the beneficiaries in the dotenv..."
+  );
+  for (let i = 0; i < AMOUNT_PER_BENEFICIARY.length; i++) {
+    console.info(`Tx ${i + 1} of ${AMOUNT_PER_BENEFICIARY.length} sent`);
+    const tx = await wallet.sendTransaction({
+      to: BENEFICIARIES[i],
+      value: ethers.utils.parseEther(AMOUNT_PER_BENEFICIARY[i]),
+      data: ethers.utils.toUtf8Bytes(
+        'Proof of Humanity missend recovery. Happy 2023!'
+      )
+    });
+    await tx.wait(CONFIRMATIONS);
+    console.info(
+      `Sent ${AMOUNT_PER_BENEFICIARY[i]} coins to ${BENEFICIARIES[i]}`
+    );
+    console.info();
+  }
+
+  console.info(
+    'Done distributing ETH. Clear dotenv beneficiaries to avoid resending.'
   );
 }
 
